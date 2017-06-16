b6af5b1555919dcc6cd56b2f0df77394dc92d4ca
Pizzacus <pizzacus@gmail.com>
2017-06-16 11:03:02 +0200

    Lots of things, read commit description
    - Added a translation system, I'll write a wiki page about it later
    - Translated the website to French (hope I didn't make any mistakes)
    - Improved the quality of the JavaScript code
    - Fixed the GitHub link
    
    I kinda did all those things at once so I didn't separate the commits

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+var javascriptLocales = {
+	"copyMessage": "Just wanted to let you know that Satania is always with you! Even inside your clipboard! We are everywhere and you should really join us!\n" +
+		"Also yes, any website can access your clipboard however they want, isn't that creepy?\n" +
+		"Regardless, Satania is the BEST WAIFU, and you should agree! http://satania.moe/",
+	"searchByVoice": "lmao, no one uses this button",
+	"searchButton": "but the results are already there =]",
+	"searchBar": "this isn't a real search bar",
+	"snedHelp": "pls send helppp",
+	"perfection": "perfection"
+}
+
 var copied = false;
 document.body.onclick = function() {
 	if (!copied) {
@@ -5,7 +16,7 @@ document.body.onclick = function() {
 
 		var previousSelection = []; // Array where the previous selections are stored
 
-		for (var i = 0; i < selection.rangeCount; i++) {
+		for (let i = 0; i < selection.rangeCount; i++) {
 			// Loops over every selections and add them to the array
 			previousSelection[i] = selection.getRangeAt(i);
 		}
@@ -17,7 +28,7 @@ document.body.onclick = function() {
 			selectionElement = document.createElement("span"); // The hidden element that will contain the text that will be selected
 
 		// Add the text to the element
-		selectionElement.innerText = "Just wanted to let you know that Satania is always with you! Even inside your clipboard! We are everywhere and you should really join us!\nAlso yes, any website can access your clipboard however they want, isn't that creepy?\nRegardless, Satania is the BEST WAIFU, and you should agree! http://satania.moe/";
+		selectionElement.innerText = javascriptLocales.copyMessage;
 
 		// Add CSS rules that should theorically prevent the hidden element from impacting the page layout in any way
 		selectionElement.setAttribute("style", "position:absolute !important;top:-9999vh !important;opacity:0 !important;height:0 !important;width:0 !important;pointer-events:none !important;z-index:-9999 !important;");
@@ -32,7 +43,7 @@ document.body.onclick = function() {
 		selection.addRange(range);
 
 		// Copy the selection to clipboard.
-		console.log(document.execCommand('copy'));
+		document.execCommand('copy');
 
 		// Clear the selection
 		selection.removeAllRanges();
@@ -40,7 +51,7 @@ document.body.onclick = function() {
 		// Remove the hidden element
 		document.body.removeChild(selectionElement);
 
-		for (var i = 0; i < previousSelection.length; i++) {
+		for (let i = 0; i < previousSelection.length; i++) {
 			// Re-select everything that was selected
 			selection.addRange(previousSelection[i]);
 		}
@@ -51,7 +62,7 @@ document.body.onclick = function() {
 
 var links = document.getElementsByTagName("a");
 
-for (var i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
 	links[i].onclick = function(e) {
 		e.preventDefault();
 		window.open(this.href, '_blank').focus();
@@ -62,39 +73,37 @@ for (var i = 0; i < links.length; i++) {
 var slideshows = document.getElementsByClassName("slideshow");
 var preloadedImages = [];
 
-for (var i = 0; i < slideshows.length; i++) {
-	(function(slideshow) {
-		var slides = slideshow.getElementsByTagName("img");
+for (let i = 0; i < slideshows.length; i++) {
+	let slideshow = slideshows[i],
+	slides = slideshow.getElementsByTagName("img");
 
-		slideshow.currentSlide = 0;
+	slideshow.currentSlide = 0;
 
-		slideshow.getElementsByClassName("source")[0].href = slides[0].getAttribute("href");
+	slideshow.getElementsByClassName("source")[0].href = slides[0].getAttribute("href");
 
-		window.setInterval(function() {
-			slides[slideshow.currentSlide].className = "";
-			slideshow.currentSlide = (slideshow.currentSlide + 1) % slides.length;
-			slides[slideshow.currentSlide].className = "shown";
+	window.setInterval(function() {
+		slides[slideshow.currentSlide].classList.remove("shown");
+		slideshow.currentSlide = (slideshow.currentSlide + 1) % slides.length;
+		slides[slideshow.currentSlide].classList.add("shown");
 
-			slideshow.getElementsByClassName("source")[0].href = slides[slideshow.currentSlide].getAttribute("href");
-		}, 2500);
+		slideshow.getElementsByClassName("source")[0].href = slides[slideshow.currentSlide].getAttribute("href");
+	}, 2500);
 
-		// Image preloading
-		for (var i = 0; i < slides.length; i++) {
-			preloadedImages.push(new Image().src = slides[i].src);
-		}
-	})(slideshows[i]);
+	// Image preloading
+	for (let i = 0; i < slides.length; i++) {
+		preloadedImages.push(new Image().src = slides[i].src);
+	}
 }
 
 var searchbar = document.getElementById("searchbar");
 
 searchbar.onclick = function(e) {
-
 	if (e.target.id === "search-by-voice") {
-		searchbar.getElementsByTagName("span")[0].innerText = "lmao, no one uses this button";
+		searchbar.getElementsByTagName("span")[0].innerText = javascriptLocales.searchByVoice;
 	} else if (e.target.id === "search-button") {
-		searchbar.getElementsByTagName("span")[0].innerText = "but the results are already there =]";
+		searchbar.getElementsByTagName("span")[0].innerText = javascriptLocales.searchButton;
 	} else {
-		searchbar.getElementsByTagName("span")[0].innerText = "this isn't a real search bar";
+		searchbar.getElementsByTagName("span")[0].innerText = javascriptLocales.searchBar;
 	}
 
 	window.setTimeout(function() {
@@ -120,6 +129,7 @@ if (isIOSChrome || (isChromium !== null && isChromium !== undefined && vendorNam
 var audioPlayingAtOnce = 0;
 
 document.getElementById("listen").onclick = function() {
+	"use strict";
 	var audio = new Audio();
 	audio.src = "perfection.mp3";
 	audio.play();
@@ -128,20 +138,21 @@ document.getElementById("listen").onclick = function() {
 		audioPlayingAtOnce--;
 
 		if (audioPlayingAtOnce < 8) {
-			document.getElementById("definition-name").innerText = "perfection";
+			document.getElementById("definition-name").innerText = javascriptLocales.perfection;
 		}
 	});
 
 	audioPlayingAtOnce++;
 
 	if (audioPlayingAtOnce > 10) {
-		document.getElementById("definition-name").innerText = "pls send helppp";
+		document.getElementById("definition-name").innerText = javascriptLocales.snedHelp;
 	}
 }
 
 document.getElementById("card-container").onclick = function(e) {
+	// It is possible that e.target is a child of the card
 	// Quality code to solve that problem :ok_hand: :ok_hand: :ok_hand:
-	if (e.target.className === "question" || e.target.className === "answer") {
+	if (e.target.classList.contain("question") || e.target.classList.contain("answer")) {
 		var card = e.target.parentElement;
 	} else if (e.target.tagName.toLowerCase() === "img" || e.target.tagName.toLowerCase() === "b") {
 		var card = e.target.parentElement.parentElement;
@@ -149,9 +160,5 @@ document.getElementById("card-container").onclick = function(e) {
 		var card = e.target;
 	}
 
-	if (card.className.match("flipped")) {
-		card.className = card.className.replace(" flipped", "");
-	} else {
-		card.className += " flipped";
-	}
+	card.classList.toggle("flipped");
 }
---
+++ src/js/translate.js
@@ -0,0 +1,127 @@
+// I could add more useful comments to my code, but I'm too lazy ¯\_(ツ)_/¯
+
+var localeSelect = document.getElementById("locale-select"),
+	options = localeSelect.getElementsByTagName("option"),
+	locales = {};
+
+if (window.Element && !Element.prototype.closest) {
+	// Thanks Mozilla <3 https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+	// Polyfill for the closest function, since a lot of browsers don't support it
+	Element.prototype.closest =
+		function(s) {
+			var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+				i,
+				el = this;
+			do {
+				i = matches.length;
+				while (--i >= 0 && matches.item(i) !== el) {};
+			} while ((i < 0) && (el = el.parentElement));
+			return el;
+		};
+}
+
+function generateTranslationTable() {
+	"use strict";
+	// Function that uses the page's content to generate a translation table
+
+	function setValue(object, path, value) {
+		// Function that changes the value of an object based on a path contained in an array
+		// For example: "setValue(obj, ["foo", "bar"], 123)" is the same as "obj.foo.bar = 123;"
+
+		// <3 https://stackoverflow.com/a/20240290
+		for (var i = 0; i < path.length - 1; i++) {
+			var key = path[i];
+			if (key in object) {
+				object = object[key];
+			} else {
+				object[key] = {};
+				object = object[key];
+			}
+		}
+		object[path[path.length - 1]] = value;
+	}
+
+
+	var translatedElements = document.querySelectorAll("[i18n]"),
+		translationTable = {};
+
+	for (let i = 0; i < translatedElements.length; i++) {
+		var element = translatedElements[i],
+			path = [element.getAttribute("i18n")],
+			text = element.innerHTML;
+
+		while (element.closest("[i18n-group]")) {
+			let groupElement = element.closest("[i18n-group]");
+
+			path.unshift(groupElement.getAttribute("i18n-group"));
+
+			element = groupElement.parentElement;
+		}
+
+		setValue(translationTable, path, text);
+	}
+
+	translationTable._javascriptLocales = javascriptLocales;
+
+	return translationTable;
+}
+
+locales.default = generateTranslationTable()
+
+function changeLocale(localeName, skipDefaultLocale) {
+	"use strict";
+	// Changes the locale, but reset it first, in case some text in a locale aren't translated in another one
+	// Example: changeLocale("fr")
+	if(!skipDefaultLocale) {
+		changeLocale("default", true);
+	}
+
+	function handleObject(locale, element) {
+		for(let value in locale) {
+			if(typeof locale[value] === "string") {
+				if (element.querySelector("[i18n=" + value + "]")) {
+					element.querySelector("[i18n=" + value + "]").innerHTML = locale[value];
+				}
+			} else {
+				if (element.querySelector("[i18n-group=" + value + "]")) {
+					handleObject(locale[value], element.querySelector("[i18n-group=" + value + "]"));
+				}
+			}
+		}
+	}
+
+	handleObject(locales[localeName], document.body);
+	javascriptLocales = locales[localeName]._javascriptLocales;
+	document.documentElement.lang = (localeName === "default") ? "en" : localeName;
+
+	if(localeName === "default") {
+		document.documentElement.lang = "en";
+		window.history.pushState('', '', window.location.pathname)
+	} else {
+		document.documentElement.lang = localeName;
+		window.location.hash = localeName;
+	}
+}
+
+for (let i = 0; i < options.length; i++) {
+	if (options[i].value !== "default") {
+		let xhr = new XMLHttpRequest();
+		xhr.open("GET", "locale/" + options[i].value + ".json", true);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				var translation = JSON.parse(xhr.responseText);
+				locales[options[i].value] = translation;
+				options[i].disabled = false;
+
+				if (window.location.hash.replace(/^\#/g, "") === options[i].value) {
+					changeLocale(options[i].value);
+				}
+			}
+		};
+		xhr.send();
+	}
+}
+
+localeSelect.onchange = function(e) {
+	changeLocale(e.target.value);
+}
